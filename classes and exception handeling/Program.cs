1716217ebea090201698dae75692a6f44f634aa7
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-08-26 18:01:40 +0530

    Add project files.

---
+++ classes and exception handeling/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_and_exception_handeling
+{
+    class Program
+    {
+        static Worker worker1 = new Worker();
+        static void Main(string[] args)
+        {
+            Console.WriteLine(worker1.Add(10, 20));
+            worker1.Print();
+            Console.ReadLine();
+        }
+    }
+} 
---
+++ classes and exception handeling/Worker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_and_exception_handeling
+{
+    class Worker
+    {
+        public void Print()
+        {
+            
+            Console.WriteLine("Hello Darshana Senevirathna");
+            Console.ReadLine();
+        }
+
+        public int Add(int x,int y)
+        {
+            int result = 0;
+
+            try {
+               result = x + y;
+            }
+            catch { }
+            return result;
+
+        }
+    }
+}
